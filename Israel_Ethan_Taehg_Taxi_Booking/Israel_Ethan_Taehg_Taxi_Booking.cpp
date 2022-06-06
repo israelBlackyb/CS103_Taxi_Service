860e56b2719f2717303b039e1fabcdd8d73e5f57
israelBlackyb <91918435+israelBlackyb@users.noreply.github.com>
2022-06-06 14:08:48 +1200

    Create

--- Israel_Ethan_Taehg_Taxi_Booking/Israel_Ethan_Taehg_Taxi_Booking.cpp
+++ Israel_Ethan_Taehg_Taxi_Booking/Israel_Ethan_Taehg_Taxi_Booking.cpp
@@ -1,20 +1,38 @@
 // Israel_Ethan_Taehg_Taxi_Booking.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "Header.h"
 
-#include <iostream>
+//landing prototype functions
+void LandingMenuSelect();
+
+int landingInput;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    DrawLine(25);
+    cout << "\nBlack & White Cab Co."<<endl; //business title
+    DrawLine(25); cout << endl;
+    DrawLine(25); cout << "\n\n";
+
+    cout << "Terms & Conditions:\n";
+    DrawLine(25);
+    cout << "\nPlease Pay\n\n";
+
+    LandingMenuSelect();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void LandingMenuSelect() {
+    DrawLine(11); cout << endl;
+    cout << "Main Menu *\n";
+    DrawLine(11); cout << endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cout << "1. Customer Login\n"<<"2. Customer Sign Up\n"<<"3. Driver Login\n"<<"4. Driver Registration\n"<<"5. Admin Options\n";
+    cout << "Choose: "; cin >> landingInput;
+
+    switch (landingInput)
+    {
+    case 1:
+
+    default:
+        break;
+    }
+}
