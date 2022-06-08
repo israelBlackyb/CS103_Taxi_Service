a13ee2c201bab7aca4b3e5654d729df15f1f0678
Ethan b <ethangaba@gmail.com>
2022-06-09 01:54:19 +1200

    first half of logged in is done.(UI)
    checks and individual passwords frame work leading to correct account types is done. now the individual admin account, driver account template, and user account template only needs to be done
    had to add via upload. idk what's going on

--- IB_EB_TH_logged_in_pages/IB_EB_TH_logged_in_pages/IB_EB_TH_logged_in_pages.cpp
+++ IB_EB_TH_logged_in_pages/IB_EB_TH_logged_in_pages/IB_EB_TH_logged_in_pages.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <CMATH>
@@ -8,8 +7,8 @@
 using namespace std;
 #include <vector>
 
-
-void mainMenuTemplate()
+//the Menu template I made.
+/*void mainMenuTemplate()
 {
 	bool run = true;
 	int menuOption;
@@ -18,9 +17,9 @@ void mainMenuTemplate()
 	//DrawLine()
 
 
-	while (true)
+	while (run == true)
 	{
-		int menuOption;
+		int menuOption = 0;
 		try
 		{
 			cin >> menuOption;
@@ -53,7 +52,7 @@ void mainMenuTemplate()
 		cout << "\nlogin fuction 2\n";
 		break;
 	case 4:
-		return 0;
+		run = false;
 		break;
 
 
@@ -64,25 +63,176 @@ void mainMenuTemplate()
 		while (true)
 		{
 			cout << "\nwait\n";
-			cin >> menuOption;
+			int wait;
+			cin >> wait;
 		}
 	}
+}*/
+
+//accounts
+void admin()
+{
+
 }
+void driver(string driverID)
+{
 
+}
+void user(string userName)
+{
 
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// initializing Varables 
+	bool runLogged = true;
+	string adminPas = "CrazyTaxi";
+	string pasInput;
+	string userName;
+	string driverID;
+	//accountID should get the account type from the main menu as a Char. 'a' = Admin, 'd' = Driver(operator), 'u' = User. !must be replaced!
+	char accountType = 'x';
+	cout << "\nInput test type: ";
+	cin >> accountType;
+	while (runLogged == true)
+	{
+		bool search = true;
+		try
+		{
+			if (accountType != 'a' && accountType != 'd' && accountType != 'u')
+			{
+				throw(accountType);
+			}
+		}
+		catch (char accountType)
+		{
+			cout << "\nERROR: accountType Invalid";
+		}
+		switch (accountType) {
+		case 'a':
+			cout << "\nPlease Enter Admin password: ";
+			cin >> pasInput;
+			for (int i = 0; i < 3; i++)
+			{
+				if (pasInput == adminPas)
+				{
+					admin();
+					i = 3;
+				}
+				else
+				{
+					cout << "\nIncorrect password.\n" << 3 - i << " Tries left \nPlease Enter correct Admin password: ";
+					cin >> pasInput;
+				}
+			}
+			runLogged = false;
+			break;
+		case 'd': {
+
+
+			//supposed to get total amount of drivers !must be replaced!
+			int driverCount = 10;
+			// string That Reads A File Or Struct That Holds All Driver Information And Gets The IDList !must be replaced!
+			string driverIDlist[10] = { "dr1", "dr2", "dr3", "dr4", "dr5" , "dr6" , "dr7" , "dr8" , "dr9" , "dr10"};
+			int dCounter = 0;
+			cout << "\nPlease enter your Driver ID: ";
+			cin >> driverID;
+			while (dCounter <= driverCount && search == true)
+			{
+				if (driverID == driverIDlist[dCounter])
+				{
+					search = false;
+				}
+				dCounter++;
+			}
+			if (dCounter > driverCount)
+			{
+				cout << "\nNo such ID exists.\n";
+			}
+			else 
+			{
+				driver(driverID);
+			}
+			runLogged = false;
+			break;
+		}
+		case 'u': {
+			//supposed to get total amount of users!must be replaced!
+			int userCount = 10;
+			// string That Reads A File Or Struct That Holds All Driver Information And Gets The IDList !must be replaced!
+			string userNamesList[10] = { "u1", "u2", "u3", "u4", "u5" , "u6" , "u7" , "u8" , "u9" , "u10" };
+			//place holder for aformentioned File Or Struct but storing passwords!must be replaced!
+			string userpasswordList[10] = { "p1", "p2", "p3", "p4", "p5" , "p6" , "p7" , "p8" , "p9" , "p10" };
+			int uCounter = 0;
+			try 
+			{
+				cout << "\nPlease enter you username: ";
+				cin >> userName;
+				while (uCounter <= userCount && search == true)
+				{
+					if (userName == userNamesList[uCounter])
+					{
+						search = false;
+					}
+					uCounter++;
+				}
+				//string userpassword [uCounter];
+				string password = userpasswordList[uCounter];
+				cout << "\nPlease Enter your password " << userName << ": ";
+				cin >> pasInput;
+				for (int i = 0; i < 3; i++)
+				{
+					if (pasInput == password)
+					{
+						user(userName);
+						i = 3;
+					}
+					else
+					{
+						cout << "\nIncorrect password.\n" << 3 - i << " Tries left \nPlease Enter correct password: ";
+						cin >> pasInput;
+					}
+				}
+				if (uCounter > userCount)
+				{
+					throw(userName);
+				}
+				
+			}
+			catch(string userName)
+			{
+				cout << "\nThe username '"<< userName <<"' doesn't exist.\n";
+				runLogged = false;
+				break;
+			}
+			//
+			for (int i = 0; i < 3; i++)
+			{
+
+			}
+			runLogged = false;
+			break;
+		}
+		default:
+			runLogged = false;
+			break;
+		}
+	}
+
+
+
+
+
+
+
+
+
+	//For observation perpouses, remove  once code is ready
+	while (true)
+	{
+		cout << "\nwait\n";
+		int wait;
+		cin >> wait;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
