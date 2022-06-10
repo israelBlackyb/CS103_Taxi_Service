41c49de69ec4e6dffa4e087d6ff05e1927d4918a
ethanbyarugaba <ethangaba@gmail.com>
2022-06-10 19:35:30 +1200

    Booking page is finished (UI and conditional)

--- IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page.cpp
+++ IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page.cpp
@@ -13,15 +13,16 @@ int main()
     bool run = true;
     int month;
     int day;
+    int hour;
     int minute;
 
-    while (run == true) {
+    while (run == true) 
+    {
         cout << "\nHere are all avaliable dates:\n\n";
         //read Read Trip_booking !must be replaced!
         cout << "\n\nAll dates are stored in number date format\nMonth, Days, Hours, and Minutes (eg Mon/d/h/m)";
         cout << "\n\nPlease choose an available date\n";
-        bool confirm1 = false;
-        while (confirm1 == false) 
+        while (true) 
         {
             
             while (true) {
@@ -60,7 +61,24 @@ int main()
             }
             while (true) {
                 try {
-                    cout << "\nDay: ";
+                    cout << "\nHour (24 style, 1-12 is AM 13-24 is PM): ";
+                    cin >> hour;
+                    if (hour >= 1 && hour <= 24)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        throw(hour);
+                    }
+                }
+                catch (int hour) {
+                    cout << "\nInvalid input!\n" << hour << " is not a valid time.\nPlease enter a valid hour.\n";
+                }
+            }
+            while (true) {
+                try {
+                    cout << "\nMinute: ";
                     cin >> minute;
                     if (minute >= 0 && minute <= 60)
                     {
@@ -72,17 +90,47 @@ int main()
                     }
                 }
                 catch (int minute) {
-                    cout << "\nInvalid input!\n" << minute << " is not a valid day.\nPlease enter a valid month.\n";
+                    cout << "\nInvalid input!\n" << minute << " is not a valid time.\nPlease enter a valid minute.\n";
                 }
             }
-
-
-
-            cout << "check 1";
+            char confirm;
+            //condintoal statmente thr reqiures ALOT of information from the booking_file !must be replaced!
+            while (true) {
+                try {
+                    if (minute < 10) {
+                        cout << "\nAre you sure you would like to book the " << month << " Month on the" << day << " Day at" << hour << ":0" << minute << "?\n(y/n): ";
+                    }
+                    else {
+                        cout << "\nAre you sure you would like to book the " << month << " Month on the" << day << " Day at" << hour << ":" << minute << "?\n(y/n): ";
+                    }
+                    cin >> confirm;
+                    confirm = tolower(confirm);
+                    if (confirm != 'y' && confirm != 'n') {
+                        throw(confirm);
+                    }
+                    else if (confirm == 'y')
+                    {
+                        run = true;
+                        break;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                }
+                catch (char confirm) {
+                    cout <<"\nInvalid input!\nPlease only enter 'y' for yes and 'n' for no.\n";
+                }
+            }
         }
 
 
     }
-
+    // this is where all the information should be transfered to the booking_file !must be replaced!
+    int booking_file_ID = rand() % 10 + 1; //!must be replaced!
+    int booking_file_month = month; //!must be replaced!
+    int booking_file_day = day; //!must be replaced!
+    int booking_file_hour = hour; //!must be replaced!
+    int booking_file_minute = minute; //!must be replaced!
 }
 
