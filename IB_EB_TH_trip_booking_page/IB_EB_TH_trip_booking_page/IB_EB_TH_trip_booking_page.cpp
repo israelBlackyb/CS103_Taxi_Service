e2434362adafeb6672c7eb2e6fd852d4f500e405
ethanbyarugaba <ethangaba@gmail.com>
2022-06-14 22:56:17 +1200

    Booking_page 99.9% Done
    All the is required is linking it to other pages and letting it get the username

--- IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page.cpp
+++ IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page/IB_EB_TH_trip_booking_page.cpp
@@ -9,14 +9,14 @@
 #include<ctime>
 using namespace std;
 
-
+//calculated ID
 int readForID() { 
     fstream fileBooking;
     string line;
     int id = 0;
 
     
-    fileBooking.open("grades.csv", ios::in);
+    fileBooking.open("bookings.csv", ios::in);
 
     
     if (fileBooking.is_open()) {
@@ -30,7 +30,47 @@ int readForID() {
     }
     return id + 1;
 }
+//fuction that might be able to read the booking file
+void readBookingFile() {
+    //creating our varables
+    fstream fileReading;
+    vector <vector <string>> content;
+    vector <string> row;
+    string line, word;
 
+    //assigning the file to read and flag to fileIn
+    fileReading.open("booking_file.csv", ios::in);
+
+    //Determine if a file is openm
+    if (fileReading.is_open())
+    {
+        while (getline(fileReading, line))
+        {
+            row.clear();
+
+            stringstream str(line);
+
+            while (getline(str, word, ','))
+                row.push_back(word);
+            content.push_back(row);
+        }
+    }
+    else
+        cout << "Could not open the file\n";
+
+    for (int i = 0; i < content.size(); i++)
+    {
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            cout << content[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    
+    return;
+}
+//sends details to bookings.csv
 void bookTaxi(int month, int day, int hour, int minute, string name, int startStreetNumber, string startStreetName, string startSuburb, string startCity, int endStreetNumber, string endStreetName, string endSuburb, string endCity) {
     fstream fileBooking;
     int booking_id = readForID();
@@ -76,15 +116,15 @@ int main()
     string endSuburb;
     string endCity;
 
-
+    //continues running until user wishes to stop their booking attempt (they do not confirm their inputed information and refuse to try booking again) or finish booking
     while (run == true)
     {
-        cout << "\nHere are all the taken dates:\n\n";
-        //read Read Trip_booking !must be replaced!
+        //explains to user how date is formated
         cout << "\n\nAll dates are stored in number date format\nMonth, Days, Hours, and Minutes (eg Mon/d/h/m)";
         Sleep(3000);
-        cout << "\n\nPlease choose an available date\n";
+        cout << "\n\nPlease choose a date for your trip\n";
         Sleep(1000);
+        //gets user input and confirms is valid
             while (true) {
                 try {
                     cout << "\nMonth: ";
@@ -103,6 +143,7 @@ int main()
                 }
             }
             Sleep(1000);
+            //gets user input and confirms is valid
             while (true) {
                 try {
                     cout << "\nDay: ";
@@ -121,6 +162,7 @@ int main()
                 }
             }
             Sleep(1000);
+            //gets user input and confirms is valid
             while (true) {
                 try {
                     cout << "\nHour (24 style, 1-12 is AM 13-24 is PM): ";
@@ -138,6 +180,7 @@ int main()
                     cout << "\nInvalid input!\n" << hour << " is not a valid time.\nPlease enter a valid hour.\n";
                 }
             }
+            //gets user input and confirms is valid
             Sleep(1000);
             while (true) {
                 try {
@@ -157,12 +200,15 @@ int main()
                 }
             }
             Sleep(1000);
+            //gets the name for the main user of the taxi to allow drivers to know who is in charge of the trip and improve user interaction 
             cout << "\nPlease enter the name of the Main User\n:";
             cin >> name;
             Sleep(1000);
+            //advices user
             cout << "\nPlease be aware that the more details you include the easier it will be for our drives to find " << name;
             Sleep(3000);
             cout << ".\nPlease enter the Starting location.\n";
+            //gets user input and confirms is valid
             while (true) {
                 try {
                     cout << "\nStreet number:";
@@ -180,19 +226,23 @@ int main()
                 }
             }
             Sleep(1000);
+            //gets user input
             cout << "\nStreet name: ";
             cin >> startStreetName;
             Sleep(1000);
+            //gets user input
             cout << "\nSuburb name: ";
             cin >> startSuburb;
             Sleep(1000);
+            //gets user input
             cout << "\nCity name: ";
             cin >> startCity;
             Sleep(1000);
-            
+            //reminds user of TOS and the fact the this is the most important part in regards to them
             cout << "\n\nPlease also be aware that the more details you include the easier it will be for our drives to find the correct destination,\nand we are not liable for any consequences caused by incorrect information being submited as per Terms & Services.";
             Sleep(3000);
             cout << "\n\nPlease enter the Destination.\n ";
+            //gets user input and confirms is valid
             while (true) {
                 try {
                     cout << "\nStreet number:";
@@ -210,16 +260,19 @@ int main()
                 }
             }
             Sleep(1000);
+            //gets user input
             cout << "\nStreet name: ";
             cin >> endStreetName;
             Sleep(1000);
+            //gets user input
             cout << "\nSuburb name: ";
             cin >> endSuburb;
             Sleep(1000);
+            //checks if the trip remians in the same city as to not force the user to repeat themselves and checks the input
             while (true)
             {
                 try {
-                    cout << "\nIs the destination city the same??\n(y/n): ";
+                    cout << "\nIs the destination city the same?\n(y/n): ";
                     cin >> confirm;
                     confirm = tolower(confirm);
                     if (confirm != 'y' && confirm != 'n') {
@@ -227,24 +280,23 @@ int main()
                     }
                     else if (confirm == 'y')
                     {
-                        endCity == startCity;
+                        endCity = startCity;
                         break;
                     }
                     else
                     {
+                        //gets user input
                         cout << "\nCity name: ";
                         cin >> endCity;
                         break;
                     }
-                    break;
                 }
                 catch (char confirm) {
                     cout << "\nInvalid input!\nPlease only enter 'y' for yes and 'n' for no.\n";
                 }
             }
             Sleep(1000);
-
-            //condintoal statmente thr reqiures ALOT of information from the booking_file !must be replaced!
+            //asks user if all booking information is correct while displaying it and asks them to confirm if it is correct
             while (true) {
                 try {
                     if (minute < 10) {
@@ -261,6 +313,7 @@ int main()
                     }
                     cin >> confirm;
                     confirm = tolower(confirm);
+                    //gets user input and confirms is valid
                     if (confirm != 'y' && confirm != 'n') {
                         throw(confirm);
                     }
@@ -271,11 +324,13 @@ int main()
                     }
                     else
                     {
+                        // if the user denind that the information was correct, askes if user wishes to retry booking a taxi 
                         while (true)
                         {
                             try {
                                 cout << "\nWould you like to Book with diffrent information?\n(y/n): ";
                                 cin >> confirm;
+                                //gets user input and confirms is valid
                                 confirm = tolower(confirm);
                                 if (confirm != 'y' && confirm != 'n') {
                                     throw(confirm);
@@ -307,7 +362,7 @@ int main()
 
 
     }
-    //User's information actally gets booked and added to the booking file
+    //User's information actally gets booked and added to the booking file if they confirmed that the infromation they inputed was correct
     if (confirm == 'y') {
         bookTaxi(month, day, hour, minute, name, startStreetNumber, startStreetName, startSuburb, startCity, endStreetNumber, endStreetName, endSuburb, endCity);
         cout << "\n\nYour taxi has been booked for the " << month << " Month, on the " << day << " Day, at" << hour << ":" << minute << " for " << name <<",\n Thank you for choosing Black and White cab Co";
